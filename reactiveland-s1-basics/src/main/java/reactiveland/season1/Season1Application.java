190e6db072a7fa7406172ad7f28c86844f130d50
Mahdi <mohammadmahdi.amini@nordnet.se>
2022-04-07 14:25:33 +0200

    enabling reactive metrics

--- reactiveland-s1-basics/src/main/java/reactiveland/season1/Season1Application.java
+++ reactiveland-s1-basics/src/main/java/reactiveland/season1/Season1Application.java
@@ -2,12 +2,12 @@ package reactiveland.season1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.scheduler.Schedulers;
 
 @SpringBootApplication
 public class Season1Application {
 
 	public static void main(String[] args) {
-
 		Schedulers.enableMetrics();
 		SpringApplication.run(Season1Application.class, args);
 	}
