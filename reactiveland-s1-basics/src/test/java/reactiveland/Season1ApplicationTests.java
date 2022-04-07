f5dd6ab28a3f2f5c5ba378a4214c3a38d19b6b2a
Mahdi <mohammadmahdi.amini@nordnet.se>
2022-04-07 13:25:54 +0200

    S2 E1 prepration

--- reactiveland-s1-basics/src/test/java/reactiveland/Season1ApplicationTests.java
+++ reactiveland-s1-basics/src/test/java/reactiveland/Season1ApplicationTests.java
@@ -1,4 +1,4 @@
-package reactiveland.season1;
+package reactiveland;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
---
+++ reactiveland-s1-basics/src/test/java/reactiveland/season2/episode1/TheInternet.java
@@ -0,0 +1,27 @@
+package reactiveland.season2.episode1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class TheInternet {
+
+    @Test
+    void getAnAdviceFromInternet(){
+        //given a stone
+        var adviserHost = "https://api.adviceslip.com";
+        var getAdvicePath = "/advice";
+        //when
+        //todo
+        Mono<String> advice = null;
+        //then
+        // todo
+    }
+
+}
