44755d5676893cca6fca74e6d07f6d4345c5ae2b
Mahdi <mohammadmahdi.amini@nordnet.se>
2022-04-25 16:41:05 +0200

    s2 e1, the internet

--- reactiveland-s1-basics/src/test/java/reactiveland/season2/episode1/TheInternet.java
+++ reactiveland-s1-basics/src/test/java/reactiveland/season2/episode1/TheInternet.java
@@ -1,36 +1,75 @@
 package reactiveland.season2.episode1;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
-import java.time.Duration;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 
 class TheInternet {
+
+    private record Advice(Integer id, String advice){}
+    private record Body(Advice slip){}
 
     @Test
     void getAnAdviceFromInternet(){
         //given
         var adviserHost = "https://api.adviceslip.com";
-        var getAdvicePath = "/advice";
+        var getAdvicePath = "/advice/81";
+        ObjectMapper objectMapper = new ObjectMapper();
         //when
-        //todo
-        Mono<String> advice = null;
+        Mono<String> advice = WebClient.builder()
+                .baseUrl(adviserHost)
+                .build()
+                .get()
+                .uri(getAdvicePath)
+                .accept(MediaType.TEXT_HTML)
+                .retrieve()
+                .bodyToMono(String.class)
+                .map(stringBody -> {
+                    try {
+                        return objectMapper.readValue(stringBody, Body.class);
+                    } catch (JsonProcessingException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .map(body -> body.slip.advice)
+                .doOnError(e -> System.out.println("Error!!" + e));
         //then
-        // todo
+        StepVerifier.create(advice)
+                .expectNext("Age is of no importance, unless you are a cheese.")
+                .expectComplete()
+                .verify();
     }
+
+    private record Todo(int id, int user_id, String title, String date, String status){}
 
     @Test
     void countCompletedTodos(){
         //given
         var todosHost = "https://gorest.co.in/public/v2";
         var todosPath = "/todos";
-        //when todo
-        //then todo
+        ObjectMapper objectMapper = new ObjectMapper();
+        //when
+        Mono<Long> numberOfTodos = WebClient.builder()
+                .baseUrl(todosHost)
+                .build()
+                .get()
+                .uri(todosPath)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToFlux(Todo.class)
+                .map(Todo::status)
+                .count()
+                .doOnError(e -> System.out.println("Error!!" + e));
+        //then
+        StepVerifier.create(numberOfTodos)
+                .expectNext(20L)
+                .expectComplete()
+                .verify();
     }
 
 }
