1cc091b68b7c8ba0b38a702252c82e1cd833e2e3
Mahdi <mohammadmahdi.amini@nordnet.se>
2022-05-02 11:27:18 +0200

    jfokus

--- reactiveland-s1-basics/src/test/java/reactiveland/season2/episode1/TheInternet.java
+++ reactiveland-s1-basics/src/test/java/reactiveland/season2/episode1/TheInternet.java
@@ -5,9 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
-
 
 class TheInternet {
 
@@ -68,6 +68,34 @@ class TheInternet {
         //then
         StepVerifier.create(numberOfTodos)
                 .expectNext(20L)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    void randomSentence(){
+        var randomWordsMono = WebClient.builder()
+                .baseUrl("https://random-num-x5ht4amjia-uc.a.run.app")
+                .build()
+                .get()
+                .uri("/")
+                .retrieve()
+                .bodyToMono(String.class)
+                .map(Integer::valueOf)
+                .flatMapMany(randomNumber -> Flux.range(1, randomNumber))
+                .flatMap(ignore -> WebClient.builder()
+                        .baseUrl("https://random-word-x5ht4amjia-uc.a.run.app")
+                        .build()
+                        .get()
+                        .uri("/")
+                        .retrieve()
+                        .bodyToMono(String.class)
+                )
+                .reduce((s, s2) -> s.concat(" ").concat(s2))
+                .log();
+
+        StepVerifier.create(randomWordsMono)
+                .expectNext(" wrong ")
                 .expectComplete()
                 .verify();
     }
