3a2c665e9e3110e41d7f05e07e448c0d054f1a83
Mahdi <mohammadmahdi.amini@nordnet.se>
2022-11-25 22:08:38 +0100

    experiment

---
+++ experiment/servlet-client/src/test/java/reactiveland/experiment/servlet/ServletApplicationTests.java
@@ -0,0 +1,13 @@
+package reactiveland.experiment.servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
