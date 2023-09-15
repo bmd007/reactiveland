6a883a1f512eb65cd64984a660bd7453aace2cde
Mahdi <bmd579@gmail.com>
2023-09-15 23:18:35 +0200

    keepMe

--- blog/blog-application/lib/main.dart
+++ blog/blog-application/lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Reactiveland blog: Mahdi',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
