ebc429e4e290e99a9f526bc2ef21183da82aedbf
Mahdi <bmd579@gmail.com>
2023-09-21 00:20:33 +0200

    experiments

--- experiments/event-only/cancel-reservation-on-payment-timeout/src/main/java/io/github/bmd007/reactiveland/CancelReservationOnPaymentTimeoutApplication.java
+++ experiments/event-only/cancel-reservation-on-payment-timeout/src/main/java/io/github/bmd007/reactiveland/CancelReservationOnPaymentTimeoutApplication.java
@@ -1,8 +1,14 @@
 package io.github.bmd007.reactiveland;
 
+import io.github.bmd007.reactiveland.configuration.KafkaEventProducer;
+import io.github.bmd007.reactiveland.event.Event;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.event.EventListener;
+import reactor.core.publisher.Flux;
+
+import java.util.UUID;
 
 @SpringBootApplication
 public class CancelReservationOnPaymentTimeoutApplication {
@@ -11,8 +17,19 @@ public class CancelReservationOnPaymentTimeoutApplication {
 		SpringApplication.run(CancelReservationOnPaymentTimeoutApplication.class, args);
 	}
 
+	@Autowired
+	private KafkaEventProducer kafkaEventProducer;
+
 	@EventListener(org.springframework.context.event.ContextRefreshedEvent.class)
 	public void contextRefreshedEvent() {
-
+		UUID customerId1 = UUID.randomUUID();
+		UUID customerId2 = UUID.randomUUID();
+		Flux.range(0, 10)
+				.map(integer -> integer % 2 == 0 ? customerId1 : customerId2)
+				.map(UUID::toString)
+				.map(uuid -> new Event.CustomerEvent.CustomerReservedTable(uuid, "reserve-id"))
+				.flatMap(kafkaEventProducer::produceCustomerEvent)
+				.log()
+				.subscribe();
 	}
 }
--- experiments/event-only/cancel-reservation-on-payment-timeout/src/main/java/io/github/bmd007/reactiveland/configuration/KafkaEventProducer.java
+++ experiments/event-only/cancel-reservation-on-payment-timeout/src/main/java/io/github/bmd007/reactiveland/configuration/KafkaEventProducer.java
@@ -3,12 +3,13 @@ package io.github.bmd007.reactiveland.configuration;
 import io.github.bmd007.reactiveland.event.Event;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
 import java.util.Properties;
-import java.util.concurrent.ExecutionException;
 
 import static io.github.bmd007.reactiveland.serialization.CustomSerdes.CUSTOMER_EVENT_JSON_SERDE;
 import static io.github.bmd007.reactiveland.serialization.CustomSerdes.RESERVATION_CANCELLED_DUE_TO_PAYMENT_TIME_OUT_JSON_SERDE;
@@ -27,21 +28,14 @@ public class KafkaEventProducer {
                 RESERVATION_CANCELLED_DUE_TO_PAYMENT_TIME_OUT_JSON_SERDE.serializer());
     }
 
-    public void produceCustomerEvent(Event.CustomerEvent customerEvent) {
-        try {
-            var record = new ProducerRecord<>(Topics.CUSTOMER_EVENTS_TOPIC, customerEvent.key(), customerEvent);
-            customerEventKafkaProducer.send(record).get();
-        } catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
-        }
+    public Mono<RecordMetadata> produceCustomerEvent(Event.CustomerEvent customerEvent) {
+        var record = new ProducerRecord<>(Topics.CUSTOMER_EVENTS_TOPIC, customerEvent.key(), customerEvent);
+        return Mono.fromCallable(() -> customerEventKafkaProducer.send(record).get());
+
     }
 
-    public void produceReservationCancelledEvent(Event.ReservationCancelledDueToPaymentTimeOut reservationCancelled) {
-        try {
-            var record = new ProducerRecord<>(Topics.RESERVATION_EVENTS_TOPIC, reservationCancelled.key(), reservationCancelled);
-            reservationCancelledOutKafkaProducer.send(record).get();
-        } catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
-        }
+    public Mono<RecordMetadata> produceReservationCancelledEvent(Event.ReservationCancelledDueToPaymentTimeOut reservationCancelled) {
+        var record = new ProducerRecord<>(Topics.RESERVATION_EVENTS_TOPIC, reservationCancelled.key(), reservationCancelled);
+        return Mono.fromCallable(() -> reservationCancelledOutKafkaProducer.send(record).get());
     }
 }
