68e833e14bc1677c5c1f3ee9757543991190c6c0
Mahdi <bmd579@gmail.com>
2023-09-23 22:57:02 +0200

    noSurpress

--- experiments/event-only/cancel-reservation-on-payment-timeout/src/main/java/io/github/bmd007/reactiveland/CancelReservationOnPaymentTimeoutApplication.java
+++ experiments/event-only/cancel-reservation-on-payment-timeout/src/main/java/io/github/bmd007/reactiveland/CancelReservationOnPaymentTimeoutApplication.java
@@ -9,7 +9,6 @@ import org.apache.kafka.clients.producer.RecordMetadata;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -31,7 +30,7 @@ public class CancelReservationOnPaymentTimeoutApplication {
 
 
     @GetMapping("/customer1")
-    public Mono customer1ReserveAndPayInTime() {
+    public Mono<String> customer1ReserveAndPayInTime() {
         String customerId1 = UUID.randomUUID().toString();
         return kafkaEventProducer.produceEvent(new CustomerRequestedTable(customerId1, "table1"), Topics.CUSTOMER_EVENTS_TOPIC)
                 .delayElement(Duration.ofSeconds(5))
@@ -41,16 +40,12 @@ public class CancelReservationOnPaymentTimeoutApplication {
     }
 
     @GetMapping("/customer2")
-    public Mono customer2ReserveAndPayInTime() {
+    public Mono<String> customer2ReserveAndPayInTime() {
         String customerId2 = UUID.randomUUID().toString();
         return kafkaEventProducer.produceEvent(new CustomerRequestedTable(customerId2, "table2"), Topics.CUSTOMER_EVENTS_TOPIC)
                 .delayElement(Duration.ofSeconds(10))
                 .map(ignored -> new CustomerPaidForReservation(customerId2, "payment2"))
                 .flatMap(event -> kafkaEventProducer.produceEvent(event, Topics.CUSTOMER_EVENTS_TOPIC))
                 .map(RecordMetadata::topic);
-    }
-
-    @EventListener(org.springframework.context.event.ContextRefreshedEvent.class)
-    public void contextRefreshedEvent() {
     }
 }
--- experiments/event-only/cancel-reservation-on-payment-timeout/src/main/java/io/github/bmd007/reactiveland/KStreamAndKTableDefinitions.java
+++ experiments/event-only/cancel-reservation-on-payment-timeout/src/main/java/io/github/bmd007/reactiveland/KStreamAndKTableDefinitions.java
@@ -14,9 +14,7 @@ import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.Materialized;
-import org.apache.kafka.streams.kstream.Suppressed;
 import org.apache.kafka.streams.kstream.TimeWindows;
-import org.apache.kafka.streams.kstream.internals.suppress.StrictBufferConfigImpl;
 import org.apache.kafka.streams.state.WindowStore;
 import org.springframework.context.annotation.Configuration;
 
@@ -47,11 +45,7 @@ public class KStreamAndKTableDefinitions {
 
     private static TableReservation aggregation(String key, Event value, TableReservation aggregate) {
         return switch (value) {
-            case CustomerRequestedTable customerRequestedTable -> {
-                TableReservation tableReservation = aggregate.withTableId(customerRequestedTable.tableId()).awaitPayment(key);
-//                log.info("awaiting payment {}", tableReservation);
-                yield tableReservation;
-            }
+            case CustomerRequestedTable customerRequestedTable -> aggregate.withTableId(customerRequestedTable.tableId()).awaitPayment(key);
             case CustomerPaidForReservation ignored -> {
                 try {
                     yield aggregate.paidFor();
@@ -68,13 +62,10 @@ public class KStreamAndKTableDefinitions {
     public void configureStores() {
         TimeWindows timeWindows = TimeWindows.ofSizeWithNoGrace(Duration.ofSeconds(10));
         streamsBuilder.stream(Topics.CUSTOMER_EVENTS_TOPIC, EVENT_CONSUMED)
-//                .peek((key, value) -> log.info("new event {}", value))
                 .groupByKey(Grouped.with(Serdes.String(), EVENT_JSON_SERDE))
                 .windowedBy(timeWindows)
                 .aggregate(TableReservation::createTable, KStreamAndKTableDefinitions::aggregation, RESERVATION_LOCAL_KTABLE_MATERIALIZED)
-                .suppress(Suppressed.untilWindowCloses(new StrictBufferConfigImpl()))
                 .toStream()
-                //maybe add a stable (not windowed) ktable for finalized reservations here
                 .foreach((key, tableReservation) -> {
                     LocalTime startTime = ZonedDateTime.ofInstant(key.window().startTime(), ZoneId.systemDefault()).toLocalTime();
                     LocalTime endTime = ZonedDateTime.ofInstant(key.window().endTime(), ZoneId.systemDefault()).toLocalTime();
