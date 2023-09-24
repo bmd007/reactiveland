fabd2a7b756d1254941814e8108ff477d6942645
Mahdi <bmd579@gmail.com>
2023-09-24 22:59:17 +0200

    test

--- experiments/event-only/cancel-reservation-on-payment-timeout/src/test/java/io/github/bmd007/reactiveland/CancelReservationOnPaymentTimeoutApplicationTests.java
+++ experiments/event-only/cancel-reservation-on-payment-timeout/src/test/java/io/github/bmd007/reactiveland/CancelReservationOnPaymentTimeoutApplicationTests.java
@@ -6,6 +6,8 @@ import io.github.bmd007.reactiveland.dto.TableReservationDto;
 import io.github.bmd007.reactiveland.event.Event;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -16,6 +18,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
 
 import java.time.Duration;
@@ -28,6 +31,7 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @EmbeddedKafka(partitions = 1, topics = {Topics.RESERVATION_EVENTS_TOPIC, Topics.CUSTOMER_EVENTS_TOPIC})
 class CancelReservationOnPaymentTimeoutApplicationTests {
 
+    private static final Logger log = LoggerFactory.getLogger(CancelReservationOnPaymentTimeoutApplicationTests.class);
 
     @Autowired
     private KafkaEventProducer kafkaEventProducer;
@@ -39,14 +43,16 @@ class CancelReservationOnPaymentTimeoutApplicationTests {
     private WebClient webClient;
 
     @BeforeEach
-    public void beforeEach(){
+    public void beforeEach() {
         webClient = WebClient.create("http://localhost:%s".formatted(localServerPort));
     }
 
     @Test
     void contextLoads() {
         //given
-        Flux<Boolean> booleanFlux = Flux.range(0, 6)
+        Flux<String> booleanFlux = Flux.range(0, 9)
+                .subscribeOn(Schedulers.parallel())
+                .publishOn(Schedulers.parallel())
                 .delayUntil(integer -> reserveAndPayForTable())
                 .flatMap(integer ->
                         switch (integer % 3) {
@@ -55,7 +61,7 @@ class CancelReservationOnPaymentTimeoutApplicationTests {
                             case 2 -> reserveTableAndLeave();
                             default -> Flux.error(new IllegalStateException("Unexpected value: " + integer % 3));
                         }
-                );
+                ).log();
         //when
         StepVerifier.create(booleanFlux)
                 //then
@@ -64,62 +70,64 @@ class CancelReservationOnPaymentTimeoutApplicationTests {
                 .verify();
     }
 
-    private Mono<Boolean> reserveAndPayForTable() {
+    private Mono<String> reserveAndPayForTable() {
         String customerId = UUID.randomUUID().toString();
         long delay = 5L;
         String tableId = UUID.randomUUID().toString();
         String paymentId = UUID.randomUUID().toString();
+        log.info("reserveAndPayForTable for {}", customerId);
         return Mono.just(new Event.CustomerEvent.CustomerRequestedTable(customerId, tableId))
                 .flatMap(event -> kafkaEventProducer.produceEvent(event, Topics.CUSTOMER_EVENTS_TOPIC))
                 .delayElement(Duration.ofSeconds(delay))
                 .map(ignored -> new Event.CustomerEvent.CustomerPaidForReservation(customerId, paymentId))
                 .flatMap(event -> kafkaEventProducer.produceEvent(event, Topics.CUSTOMER_EVENTS_TOPIC))
-                .delayElement(Duration.ofSeconds(2))
+                .delayElement(Duration.ofSeconds(10))
                 .flatMap(ignored -> webClient.get()
                         .uri("/api/tables/reservations/%s".formatted(customerId))
                         .retrieve()
                         .bodyToMono(TableReservationDto.class)
                 )
                 .map(TableReservationDto::status)
-                .map(s -> s.equals("PAID_FOR"));
+                .map(s -> customerId + ":" + s);
     }
 
-    private Mono<Boolean> reserveTableAndLeave() {
+    private Mono<String> reserveTableAndLeave() {
         String customerId = UUID.randomUUID().toString();
         long delay = 22L;
         String tableId = UUID.randomUUID().toString();
+        log.info("reserveTableAndLeave for {}", customerId);
         return Mono.just(new Event.CustomerEvent.CustomerRequestedTable(customerId, tableId))
                 .flatMap(event -> kafkaEventProducer.produceEvent(event, Topics.CUSTOMER_EVENTS_TOPIC))
-                .delayElement(Duration.ofSeconds(2))
+                .delayElement(Duration.ofSeconds(delay))
                 .flatMap(ignored -> webClient.get()
                         .uri("/api/tables/reservations/%s".formatted(customerId))
                         .retrieve()
                         .bodyToMono(TableReservationDto.class)
                 )
                 .map(TableReservationDto::status)
-                .map(s -> s.equals("RESERVED_AWAITING_PAYMENT"));
+                .map(s -> customerId + ":" + s);
     }
 
-    private Mono<Boolean> reserveTableAndPayLate() {
+    private Mono<String> reserveTableAndPayLate() {
         String customerId = UUID.randomUUID().toString();
         long delay = 20L;
         String tableId = UUID.randomUUID().toString();
         String paymentId = UUID.randomUUID().toString();
+        log.info("reserveTableAndPayLate for {}", customerId);
         return Mono.just(new Event.CustomerEvent.CustomerRequestedTable(customerId, tableId))
                 .flatMap(event -> kafkaEventProducer.produceEvent(event, Topics.CUSTOMER_EVENTS_TOPIC))
                 .delayElement(Duration.ofSeconds(delay))
                 .map(ignored -> new Event.CustomerEvent.CustomerPaidForReservation(customerId, paymentId))
                 .flatMap(event -> kafkaEventProducer.produceEvent(event, Topics.CUSTOMER_EVENTS_TOPIC))
-                .delayElement(Duration.ofSeconds(2))
+                .delayElement(Duration.ofSeconds(10))
                 .flatMap(ignored -> webClient.get()
                         .uri("/api/tables/reservations/%s".formatted(customerId))
                         .retrieve()
                         .bodyToMono(String.class)
                         .onErrorResume(WebClientResponseException.class, exception -> Mono.just(exception.getStatusCode().toString()))
-                        .log()
-
                 )
-                .map(httpStatusCode -> httpStatusCode.equals(HttpStatus.NOT_FOUND.toString()));
+                .map(httpStatusCode -> httpStatusCode.equals(HttpStatus.NOT_FOUND.toString()))
+                .map(s -> customerId + ":" + s);
     }
 
 }
